dbbf02b6ddba8e8230034b9a32166a2708742efb
Fazliddin O'sarov <fazliddinusarov1999@gamil.com>
2025-02-04 08:37:38 +0500

    fix: user model

--- database/models.py
+++ database/models.py
@@ -10,15 +10,18 @@ class Base(DeclarativeBase):
 
 class User(Base):
     __tablename__ = "users"
-    id: Mapped[int] = mapped_column(primary_key=True)
-    is_bot: Mapped[bool] = mapped_column(default=False)
-    first_name: Mapped[str] = mapped_column(String(255), nullable=False)
-    last_name: Mapped[str] = mapped_column(String(255))
-    username: Mapped[str] = mapped_column(String(255), unique=True)
-    language_code: Mapped[str] = mapped_column(String(2))
-    is_admin: Mapped[bool] = mapped_column(default=False)
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=False)
+    is_bot: Mapped[bool] = mapped_column(default=False, nullable=False)
+    first_name: Mapped[str] = mapped_column(String(55), nullable=False)
+    last_name: Mapped[str] = mapped_column(String(55), nullable=True)
+    username: Mapped[str] = mapped_column(String(55), unique=True, nullable=True)
+    language_code: Mapped[str] = mapped_column(String(2), nullable=True)
+    is_admin: Mapped[bool] = mapped_column(default=False, nullable=False)
 
-    created_at: Mapped[datetime] = mapped_column(default=func.now())
+    created_at: Mapped[datetime] = mapped_column(default=func.now(), nullable=False)
     updated_ad: Mapped[datetime] = mapped_column(
-        default=func.now(), onupdate=func.now()
+        default=func.now(), onupdate=func.now(), nullable=False
     )
+
+    def __repr__(self) -> str:
+        return f"<User(id={self.id}, firt_name={self.first_name}), username={self.username}>"  # noqa: E501
