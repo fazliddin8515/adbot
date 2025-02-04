1c9efe1e1aebe6c7685e98770dba1fbdb8db3182
Fazliddin O'sarov <fazliddinusarov1999@gamil.com>
2025-02-04 22:09:34 +0500

    refac: user model

--- database/models.py
+++ database/models.py
@@ -13,15 +13,31 @@ class User(Base):
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=False)
     is_bot: Mapped[bool] = mapped_column(default=False, nullable=False)
     first_name: Mapped[str] = mapped_column(String(55), nullable=False)
-    last_name: Mapped[str] = mapped_column(String(55), nullable=True)
-    username: Mapped[str] = mapped_column(String(55), unique=True, nullable=True)
-    language_code: Mapped[str] = mapped_column(String(2), nullable=True)
-    is_admin: Mapped[bool] = mapped_column(default=False, nullable=False)
+    last_name: Mapped[str | None] = mapped_column(String(55), nullable=True)
+    username: Mapped[str | None] = mapped_column(String(55), unique=True, nullable=True)
+    language_code: Mapped[str | None] = mapped_column(String(2), nullable=True)
+    is_admin: Mapped[bool | None] = mapped_column(default=False, nullable=False)
 
     created_at: Mapped[datetime] = mapped_column(default=func.now(), nullable=False)
     updated_ad: Mapped[datetime] = mapped_column(
         default=func.now(), onupdate=func.now(), nullable=False
     )
+
+    def __init__(
+        self,
+        id: int,
+        is_bot: bool,
+        first_name: str,
+        last_name: str | None,
+        username: str | None,
+        language_code: str | None,
+    ) -> None:
+        self.id = id
+        self.is_bot = is_bot
+        self.first_name = first_name
+        self.last_name = last_name
+        self.username = username
+        self.language_code = language_code
 
     def __repr__(self) -> str:
         return f"<User(id={self.id}, firt_name={self.first_name}), username={self.username}>"  # noqa: E501
