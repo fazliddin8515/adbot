64ea616a453e0a549f668b392760cb0554c67faf
Fazliddin O'sarov <fazliddinusarov1999@gamil.com>
2025-02-06 21:45:55 +0500

    refac:

--- utils/db.py
+++ utils/db.py
@@ -2,13 +2,13 @@ from sqlalchemy.engine import URL
 
 
 def create_db_url(
-    drivername: str, username: str, password: str, host: str, port: str, database: str
+    drivername: str, username: str, password: str, host: str, port: int, database: str
 ) -> URL:
     return URL.create(
         drivername=drivername,
         username=username,
         password=password,
         host=host,
-        port=int(port),
+        port=port,
         database=database,
     )
